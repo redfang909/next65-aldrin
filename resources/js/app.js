3a0806075d80148f2e030153b332fa7be4ff54ba
Aldrin Cantero <aldrin.cantero@gmail.com>
2020-06-26 10:05:49 +0800

    add eslint

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,15 @@
 /* eslint-disable no-use-before-define */
 import Demo from './demo';
+import Demo2 from './demo2';
+import Demos from './dems/demos';
 require('./bootstrap');
 
+const a = new Demos();
+a.onDocScroll();
 const demo = new Demo();
 demo.onLoadWindow();
 demo.onDocScroll();
 demo.onStageClick();
+
+const demo2 = new Demo2();
+demo2.onLoadWindow();
